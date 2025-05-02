3d8738cb63954dc71c881f60c97e1432e3689f29
Akhtar Faizi Putra <akhtarerror@gmail.com>
2025-05-02 10:43:20 +0700

    Implementasi Chatbot

--- app/src/main/java/pnj/pk/pareaipk/ui/home/HomeFragment.kt
+++ app/src/main/java/pnj/pk/pareaipk/ui/home/HomeFragment.kt
@@ -1,37 +1,32 @@
 package pnj.pk.pareaipk.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import pnj.pk.pareaipk.databinding.FragmentHomeBinding
+import pnj.pk.pareaipk.ui.chatbot.ChatbotActivity
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val root = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.btnChatbot.setOnClickListener {
+            val intent = Intent(requireContext(), ChatbotActivity::class.java)
+            startActivity(intent)
         }
+
         return root
     }
 
